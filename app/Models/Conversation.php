6a1fc1934531555a70d3bcf94a1658e924c93e74
amerkb <128022288+amerkb@users.noreply.github.com>
2025-08-25 22:43:36 +0300

    v cv رؤر

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -14,6 +14,7 @@ class Conversation extends Model
     public function lastMessage()
     {
         return $this->belongsTo(Message::class, 'last_message_id', 'id')
+            ->where('hate',false)
             ->whereNull('deleted_at');
 
     }
